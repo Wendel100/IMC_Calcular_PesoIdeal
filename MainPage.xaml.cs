1c0749eb0ceb1776270edcda3150ab1efac32d5f
Wendel <renanwendel100@gmail.com>
2025-08-12 21:11:34 -0300

    version1.1.4

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,54 +5,11 @@
 
     public partial class MainPage : ContentPage
     {
-        private bool _formatandoAltura = false;
-        private bool _formatandoPeso = false;
         public MainPage()
         {
             InitializeComponent();
         }
-        private void OnAlturaTextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (_formatandoAltura) return;
-            _formatandoAltura = true;
-
-            var entry = (Entry)sender;
-            var texto = new string(entry.Text.Where(char.IsDigit).ToArray());
-
-            if (double.TryParse(texto, out double alturaCm))
-            {
-                double alturaMetros = alturaCm / 100; // 170 cm → 1.70 m
-                entry.Text = alturaMetros.ToString("N2", CultureInfo.CurrentCulture); // 2 casas decimais
-            }
-            else
-            {
-                entry.Text = "";
-            }
-
-            _formatandoAltura = false;
-        }
-
-        // PESO: 705 → 70,5 kg
-        private void OnPesoTextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (_formatandoPeso) return;
-            _formatandoPeso = true;
-
-            var entry = (Entry)sender;
-            var texto = new string(entry.Text.Where(char.IsDigit).ToArray());
-
-            if (double.TryParse(texto, out double pesoGramas))
-            {
-                double pesoKg = pesoGramas / 10; // 705 → 70.5
-                entry.Text = pesoKg.ToString("N1", CultureInfo.CurrentCulture); // 1 casa decimal
-            }
-            else
-            {
-                entry.Text = "";
-            }
-
-            _formatandoPeso = false;
-        }
+    
         private async void OnCounterClickedAsync(object sender, EventArgs e)
         {
             double peso = double.Parse(PesoEntry.Text);
@@ -62,38 +19,38 @@
             if (imc > 0 && imc <= 17)
             {
 
-                teste.Text = $"O vc esta abaixo do peso {imc.ToString("0.0")} kg";
+                teste.Text = $"esta abaixo do peso {imc.ToString("0.0")} kg";
                 await TextToSpeech.SpeakAsync(teste.Text);
 
             }
             else if (imc >= 18 && imc < 24)
             {
 
-                teste.Text = $"O vc esta dentro do peso ideal {imc.ToString("0.0")}kg";
+                teste.Text = $"esta dentro do peso ideal {imc.ToString("0.0")}kg";
                 await TextToSpeech.SpeakAsync(teste.Text);
             }
             else if (imc >= 25 && imc <= 29)
             {
 
-                teste.Text = $"O vc esta com pre obesidade {imc.ToString("0.0")}kg";
+                teste.Text = $"esta com pre obesidade {imc.ToString("0.0")}kg";
                 await TextToSpeech.SpeakAsync(teste.Text);
             }
             else if (imc >= 29 && imc <= 35)
             {
 
-                teste.Text = $"O vc esta com obesidade grau 1 {imc.ToString("0.0")}kg";
+                teste.Text = $"esta com obesidade grau 1 {imc.ToString("0.0")}kg";
                 await TextToSpeech.SpeakAsync(teste.Text);
             }
             else if (imc >= 35 && imc <= 39)
             {
 
-                teste.Text = $"O vc esta com obesidade grau 2 {imc.ToString("0.0")}kg";
+                teste.Text = $"esta com obesidade grau 2 {imc.ToString("0.0")}kg";
                 await TextToSpeech.SpeakAsync(teste.Text);
             }
             else if (imc >= 40)
             {
 
-                teste.Text = $"Estado critico!!!!!,n/ vc esta com obesidade grau 3 {imc.ToString("0.0")}kg";
+                teste.Text = $"Estado critico, esta com obesidade grau 3 {imc.ToString("0.0")}kg";
                 await TextToSpeech.SpeakAsync(teste.Text);
             }
             else
